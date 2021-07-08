87e81ee21df64e9800e270317d97691de3f8975d
Илья <primebox21@gmail.com>
2021-07-08 18:49:59 +0700

    corse continue
    Черновой  вариант скрипта DDL-команд

--- course/book_shop_scr.sql
+++ course/book_shop_scr.sql
@@ -1,16 +1,16 @@
-DROP DATABASE book_shop IF exists;
-CREATE DATABASE book_shop;
-USE book_shop;
+-- DROP DATABASE IF exists book_shop;
+-- CREATE DATABASE book_shop;
+-- USE book_shop;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	id serial PRIMARY KEY,
-	login varchar(100) NOT NULL UNIQUE,
-	password_hash varchar(100),
-	email varchar(100) NOT NULL UNIQUE,	
+	id SERIAL PRIMARY KEY,
+	login VARCHAR(100) NOT NULL UNIQUE,
+	password_hash VARCHAR(100),
+	email VARCHAR(100) NOT NULL UNIQUE,	
 	phone BIGINT UNSIGNED NOT NULL UNIQUE,
-	firstname varchar(100),
-	lastname varchar(100),	
+	firstname VARCHAR(100),
+	lastname VARCHAR(100),	
 	birthday_at DATE,
 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
  	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -19,68 +19,128 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS genres;
 CREATE TABLE genres (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(100)	 
-)COMMENT 'Каталоги книг';
+) COMMENT 'Жанры';
 
 DROP TABLE IF EXISTS collections;
 CREATE TABLE collections (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(100)
 ) COMMENT 'Подборки';
 
 DROP TABLE IF EXISTS publishing_houses;
 CREATE TABLE publishing_houses (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(100)
 ) COMMENT 'Издательства';
 
 DROP TABLE IF EXISTS autors;
 CREATE TABLE autors (
-	id serial PRIMARY KEY,
-	firstname varchar(100),
-	lastname varchar(100)
+	id SERIAL PRIMARY KEY,
+	firstname VARCHAR(100),
+	lastname VARCHAR(100)
 ) COMMENT 'Авторы';
 
 -- Может быть лишнее
---DROP TABLE IF EXISTS age_limits;
---CREATE TABLE age_limits (
---	id serial PRIMARY KEY,
---	age_limit tinyint,
---	description varchar(255)
---) COMMENT 'Возрастные ограничения';
+DROP TABLE IF EXISTS age_limits;
+CREATE TABLE age_limits (
+	id SERIAL PRIMARY KEY,
+	age_limit tinyint,
+	description VARCHAR(255)
+) COMMENT 'Возрастные ограничения';
 
 -- Может быть лишнее
 DROP TABLE IF EXISTS covers;
 CREATE TABLE covers (
-	id serial PRIMARY KEY,
-	filename varchar(255),
-	SIZE INT,
+	id SERIAL PRIMARY KEY,
+	filename VARCHAR(255),
+	size INT,
 	metadata JSON
 ) COMMENT 'Обложки книг';
 
 DROP TABLE IF EXISTS books;
 CREATE TABLE books (
-	id serial PRIMARY KEY,
-	name varchar(255),
+	id SERIAL PRIMARY KEY,	
+	genre_id BIGINT UNSIGNED,
+  	collection_id BIGINT UNSIGNED,
+  	publishing_house_id BIGINT UNSIGNED,
+  	autor_id BIGINT UNSIGNED,
+  	age_limit_id BIGINT UNSIGNED,
+  	cover_id BIGINT UNSIGNED,
+  	name VARCHAR(255),
 	description tinytext,
-	price decimal (11,2),
-	age_limit tinyint,
+	price DECIMAL (11,2),
+-- 	age_limit tinyint,
 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  	genre_id BIGINT UNSIGNED NOT NULL,
-  	collection_id BIGINT UNSIGNED NOT NULL,
-  	publishing_house_id BIGINT UNSIGNED NOT NULL,
-  	autor_id BIGINT UNSIGNED NOT NULL,
-  	cover_id BIGINT UNSIGNED NOT NULL,
-  	
+  	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  	  	
   	FOREIGN KEY (genre_id) REFERENCES genres(id),
   	FOREIGN KEY (collection_id) REFERENCES collections(id),
   	FOREIGN KEY (publishing_house_id) REFERENCES publishing_houses(id),
   	FOREIGN KEY (autor_id) REFERENCES autors(id),
-  	FOREIGN KEY (cover_id) REFERENCES covers(id),
-  	
+  	FOREIGN KEY (age_limit_id) REFERENCES age_limits(id),
+  	FOREIGN KEY (cover_id) REFERENCES covers(id)  	
 ) COMMENT 'Книги';
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	user_id BIGINT UNSIGNED,
+-- 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		
+	FOREIGN KEY (user_id) REFERENCES users(id)
+) COMMENT 'Заказы';
+
+DROP TABLE IF EXISTS orders_books;
+CREATE TABLE orders_books (
+	id SERIAL PRIMARY KEY,	
+	order_id BIGINT UNSIGNED,
+	book_id BIGINT UNSIGNED,
+	total INT DEFAULT 1,
+-- 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		
+	FOREIGN KEY (order_id) REFERENCES orders(id),
+	FOREIGN KEY (book_id) REFERENCES books(id)
+) COMMENT 'Состав заказа';
+
+DROP TABLE IF EXISTS storehouses;
+CREATE TABLE storehouses (
+	id SERIAL PRIMARY KEY,
+	address VARCHAR(255)
+-- 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT 'Склады';
+
+DROP TABLE IF EXISTS storehouses_books;
+CREATE TABLE storehouses_books (
+	id SERIAL PRIMARY KEY,	
+	storehouse_id BIGINT UNSIGNED,
+	book_id BIGINT UNSIGNED,
+	value BIGINT UNSIGNED,
+-- 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	
+	FOREIGN KEY (storehouse_id) REFERENCES storehouses(id),
+	FOREIGN KEY (book_id) REFERENCES books(id)
+) COMMENT 'Запасы на складе';
+
+DROP TABLE IF EXISTS disconts;
+CREATE TABLE disconts (
+	id SERIAL PRIMARY KEY,
+	user_id BIGINT UNSIGNED,
+	book_id BIGINT UNSIGNED,
+	discont FLOAT UNSIGNED,
+	started_at DATETIME,
+	finished_at DATETIME,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  	
+  	FOREIGN KEY (user_id) REFERENCES users(id),
+  	FOREIGN KEY (book_id) REFERENCES books(id)
+) COMMENT 'Скидки';
 
 
 
--- course/readme.md
+++ course/readme.md
@@ -1,22 +1,88 @@
 Описание
 
 База данных 'book_shop' содержит таблицы:
-	1 users;	
-	2 genres
+
+	1 users:
+		id,
+		login,
+		password_hash,
+		email,	
+		phone,
+		firstname,
+		lastname,	
+		birthday,
+		created_at,
+	 	updated_at;	
+	 	
+	2 genres:
 		id
 		name;	
-	3 collections;
-	4 publishing_houses;
-	5 autors;	
-	6 covers; ?
-	7 books:
+		
+	3 collections:
 		id,
-		name,
-		description
-		catalog_id;
-	8 orders;
-	9 orders_books;
-	10 storehouses;
-	11 storehouses_books;
-	12 disconts
+		name;
+		
+	4 publishing_houses
+		id,
+		name;
+		
+	5 autors:
+		id,
+		firstname,
+		lastname;	
+		
+	6 age_limits:		?
+		id,
+		age_limit,
+		description;
+		
+	7 covers:			?
+		id,
+		filename,
+		size,
+		metadata;
+		
+	8 books:
+		id,	
+		genre_id,
+	  	collection_id,
+	  	publishing_house_id,
+	  	autor_id,
+	  	age_limit_id,
+	  	cover_id,
+	  	name,
+		description,
+		price,	
+		created_at,
+	  	updated_at;
+	  	
+	9 orders:
+		id,
+		user_id;
+		
+	10 orders_books:
+		id,	
+		order_id,
+		book_id,
+		total;
+		
+	11 storehouses:
+		id,
+		address;
+		
+	12 storehouses_books:
+		id,	
+		storehouse_id,
+		book_id,
+		value;
+		
+	13 disconts:
+		id,
+		user_id,
+		book_id,
+		discont,
+		started_at,
+		finished_at,
+		created_at,
+	  	updated_at.
 	
