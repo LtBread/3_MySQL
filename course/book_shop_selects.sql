945bba9c61ea9151d47d0a79c2c0dd856f127237
Илья <primebox21@gmail.com>
2021-07-09 18:51:12 +0700

    update

--- course/book_shop_selects.sql
+++ course/book_shop_selects.sql
@@ -12,7 +12,30 @@ SELECT * FROM storehouses;
 SELECT * FROM storehouses_books;
 SELECT * FROM disconts;
 
+SELECT id, name
+FROM books;
+
 SELECT b.id, b.name, a.lastname, al.age_limit 
 FROM books b 
 JOIN authors a ON b.autor_id = a.id 
-JOIN age_limits al ON b.age_limit_id = al.id
+JOIN age_limits al ON b.age_limit_id = al.id;
+
+SELECT o.id , o.user_id, u.login 
+FROM orders o
+JOIN users u ON o.user_id = u.id;
+
+SELECT 
+	concat(u.firstname, ' ', u.lastname), 
+	b.name, 
+	concat(a.firstname, ' ', a.lastname) AS 'authors'
+FROM users u 
+JOIN orders o ON o.user_id = u.id 
+JOIN orders_books ob ON ob.order_id = o.id 
+JOIN books b ON b.id = ob.book_id 
+JOIN authors a ON b.autor_id = a.id;
+
+-- как бы запросить остатки по всем складам?
+SELECT b.name, sb.value 
+FROM books b 
+JOIN storehouses_books sb ON sb.book_id = b.id
+WHERE sb.storehouse_id = 1;
